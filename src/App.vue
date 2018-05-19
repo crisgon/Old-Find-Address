68875484ca2f252a52d94000c4fede04b79529b0
crisgon <kristiano.gon@gmail.com>
2018-05-18 23:51:14 -0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <formulario/>
+  </div>
+</template>
+
+<script>
+import Formulario from './Components/Formulario/formulario';
+export default {
+  name: 'app',
+  components: {
+    formulario: Formulario
+  },
+
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+  body {
+    background-color: #2ed573;
+  }
+</style>
---
+++ src/Components/Formulario/formulario.vue
@@ -0,0 +1,36 @@
+<template>
+  <form class="formulario">
+    <label for="cep" class="formulario-label">CEP:</label>
+    <input type="text" class="formulario-input" placeholder="CEP" id="cep">
+  </form>
+</template>
+
+<script>
+  export default{
+  }
+</script>
+
+<style scoped>
+  .formulario {
+    width: 300px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .formulario-label {
+    font-weight: bold;
+    padding: .3em 0;
+  }
+
+  .formulario-input {
+    width: 100%;
+    box-sizing: border-box;
+    padding: 1em;
+    margin: .5em 0;
+    border-radius: 6px;
+    border: 0;
+    outline: 0;
+  }
+</style>
